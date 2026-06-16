ce0642ec7453323750e0a41ec8c8ef42bea3ff83
Dereck Ndlovu <ndlovudereck01@gmail.com>
2026-06-16 15:23:14 +0200

    comment:import dj_database_url

--- tobacco_app/settings.py
+++ tobacco_app/settings.py
@@ -4,7 +4,7 @@ Django settings for tobacco_app project.
 
 import os
 from pathlib import Path
-import dj_database_url
+#import dj_database_url
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
